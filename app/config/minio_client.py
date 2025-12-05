c88ed72357070f517761630efab1d20bffdb8ff9
vivek <vivek.pydev@gmail.com>
2025-12-05 17:31:52 +0000

    minio client

---
+++ app/config/minio_client.py
@@ -0,0 +1,19 @@
+import os
+from minio import Minio
+
+MINIO_HOST = os.getenv("MINIO_HOST", "minio")
+MINIO_PORT = os.getenv("MINIO_PORT", "9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minio")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+
+client = Minio(
+    f"{MINIO_HOST}:{MINIO_PORT}",
+    access_key=MINIO_ACCESS_KEY,
+    secret_key=MINIO_SECRET_KEY,
+    secure=False
+)
+
+def ensure_bucket(bucket_name):
+    found = client.bucket_exists(bucket_name)
+    if not found:
+        client.make_bucket(bucket_name)
