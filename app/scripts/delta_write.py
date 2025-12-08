f87e6262458dd99ad68b4ad17a2d274212be21d1
vivek <vivek.pydev@gmail.com>
2025-12-08 17:57:08 +0000

    delta write

---
+++ app/scripts/delta_write.py
@@ -0,0 +1,17 @@
+import os
+import pyspark as pd
+import boto3
+from deltalake.writer import DeltaWriter
+
+
+# S3 / Minio config via env
+S3_ENDPOINT = os.getenv("S3_ENDPOINT", "http://minio:9000")
+BUCKET_NAME = os.getenv("DELTA_S3_BUCKET", "delta")
+
+TABLE_PATH = f"s3://{BUCKET_NAME}/crypto/bitcoin_ohlcv"
+
+def write_delta(df: pd.DataFrame):
+    if df.empty:
+        return
+    
+    write_deltalake(TABLE_PATH, df, mode="append")
