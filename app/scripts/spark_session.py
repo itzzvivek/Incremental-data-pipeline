83fa2645a96eff11be6a05c0d758786a2f05b023
vivek <vivek.pydev@gmail.com>
2025-12-20 17:50:05 +0000

    update spark session

--- app/scripts/spark_session.py
+++ app/scripts/spark_session.py
@@ -2,18 +2,38 @@ from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 import os
 
-def get_spark(app_name: str):
-    return (
-        SparkSession.builder
-        .appName("incremental_data")
 
-        # S3A + MinIO
-        .config("spark.hadoop.fs.s3a.endpoint", os.getenv("MINIO_ENDPOINT", "http://localhost:9000"))
-        .config("spark.hadoop.fs.s3a.access.key", os.getenv("MINIO_ACCESS_KEY", "minioadmin"))
-        .config("spark.hadoop.fs.s3a.secret.key", os.getenv("MINIO_SECRET_KEY", "minioadmin"))
+def get_spark(app_name: str = "incremental_data"):
+    builder = (
+        SparkSession.builder
+        .appName(app_name)
+
+        # ---------- S3A + MinIO ----------
+        .config(
+            "spark.hadoop.fs.s3a.endpoint",
+            os.getenv("S3_ENDPOINT", "http://minio:9000")
+        )
+        .config(
+            "spark.hadoop.fs.s3a.access.key",
+            os.getenv("AWS_ACCESS_KEY_ID", "minioadmin")
+        )
+        .config(
+            "spark.hadoop.fs.s3a.secret.key",
+            os.getenv("AWS_SECRET_ACCESS_KEY", "minioadmin")
+        )
         .config("spark.hadoop.fs.s3a.path.style.access", "true")
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+
+        # ---------- Delta Lake ----------
+        .config(
+            "spark.sql.extensions",
+            "io.delta.sql.DeltaSparkSessionExtension"
+        )
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog"
+        )
     )
 
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
