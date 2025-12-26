ea3135ba65722820a11a2e33d8f3f52ac1f9b4be
vivek <vivek.pydev@gmail.com>
2025-12-26 08:33:24 +0000

    update run_pipeline

--- app/scripts/run_pipeline.py
+++ app/scripts/run_pipeline.py
@@ -2,9 +2,11 @@ from fetch_raw import fetch_raw
 from fetch_incremental import build_incremental
 from delta_write import write_raw, write_clean
 from metadata import update_metadata
+from spark_session import get_spark
 
 
 def run_once():
+    spark = get_spark("Run Pipeline")
     raw_df = fetch_raw()
     write_raw(raw_df)
 
@@ -20,3 +22,6 @@ def run_once():
     update_metadata(max_time)
 
     print(f"Pipeline run complete. Updated up to {max_time}.")
+
+if __name__ == "__main__":
+    run_once()
